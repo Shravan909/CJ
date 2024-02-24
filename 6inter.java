db2f20092721bc9f4cd73e20e87cbb16906cd82a
nicorobin0508 <117674705+nicorobin0508@users.noreply.github.com>
2024-02-24 19:00:52 +0530

    Create 6inter.java

---
+++ 6inter.java
@@ -0,0 +1,27 @@
+interface I1
+{
+  void show1();
+}
+interface I2
+{
+  void show2();
+}
+public class Inter  implements I1,I2
+{
+  
+  public void show1()
+  {
+    System.out.println("1st show function");
+  }
+  public void show2()
+  {
+    System.out.println("2nd show function");
+  }
+  
+  public static void main(String[] args)
+  {
+    Main obj1 = new Main();
+    obj1.show1();
+    obj1.show2();
+  }
+}
