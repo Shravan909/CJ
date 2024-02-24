28bbaa236c22b52a8c025c0acb6dbdaece27581f
nicorobin0508 <117674705+nicorobin0508@users.noreply.github.com>
2024-02-24 18:57:48 +0530

    Create static.java

---
+++ static.java
@@ -0,0 +1,12 @@
+//Write a program to create a class and implement the concepts of Static methods
+public class Static1
+{
+  static void Add(int a , int b)
+  {
+    System.out.println("ADDITION : "+(a+b));
+  }
+  public static void main(String[] args)
+  {
+    Add(20,30);
+  }
+}
